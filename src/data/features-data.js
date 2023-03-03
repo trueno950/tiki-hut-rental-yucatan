b0e40e80f0ff3803eeeb6f41a8824276ed91a142
trueno950 <mayjoel471@gmail.com>
2023-03-03 13:36:57 -0600

    Primer commit

---
+++ src/data/features-data.js
@@ -0,0 +1,31 @@
+import {
+  StarIcon,
+  ArrowPathIcon,
+  FingerPrintIcon,
+} from "@heroicons/react/24/solid";
+
+export const featuresData = [
+  {
+    color: "blue",
+    title: "Producto 1",
+    icon: StarIcon,
+    description:
+      "lorem irf erioierf yuerieruhfg",
+  },
+  {
+    color: "red",
+    title: "Producto 2",
+    icon: ArrowPathIcon,
+    description:
+      "Krewgfregfegegege ewroiurewgfiurew egouiege ioefroiufe",
+  },
+  {
+    color: "teal",
+    title: "Producto 3",
+    icon: FingerPrintIcon,
+    description:
+      "oiherore oiedtrfioufiouf gpogo fufu",
+  },
+];
+
+export default featuresData;
